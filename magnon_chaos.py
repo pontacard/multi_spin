421a6b4575b08b619ed9960b5e877a00e5a1f640
pontacard <81417109+pontacard@users.noreply.github.com>
2023-11-08 14:41:53 +0900

    Add files via upload

--- magnon_chaos.py
+++ magnon_chaos.py
@@ -4,20 +4,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from typeZ import Spin_wave
+from sin_spin_wave import Sin_Spin_wave
 
 class Phase_space(Spin_wave):
-    def __init__(self,alpha,gamma,B,S0,t,t_eval,Kx,Ky,Kz,beta,pulse,N,J):
-        super().__init__(alpha, gamma, B, S0, t, t_eval, Kx,Ky,Kz,beta,pulse,N,J)
+    def __init__(self,alpha,edge_alpha,gamma,B,S0,t,t_eval,Kx,Ky,Kz,beta,pulse,N,J):
+        super().__init__(alpha,edge_alpha, gamma, B, S0, t, t_eval, Kx,Ky,Kz,beta,pulse,N,J)
 
-    def make_phase_graph(self,i,ax1,ax2):      #二次元のぐらふを作る。iはi番目のスピン、axはどの軸をとるか
+    def solving(self):
         self.S0 = np.reshape(self.S0, (1, -1))
         self.S0 = list(self.S0[0])
-        #self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
+        # self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
         print(self.S0)
 
         self.Sol = sc.integrate.solve_ivp(self.func_S, self.t, self.S0, t_eval=self.t_eval)
         self.S = self.Sol.y
         self.spin_log = np.reshape(self.S, (self.N, 3, -1))
+
+
+    def make_phase_graph(self,i,ax1,ax2):      #二次元のぐらふを作る。iはi番目のスピン、axはどの軸をとるか
         t = self.t_eval
         x = self.spin_log[i][0]
         y = self.spin_log[i][1]
@@ -26,6 +30,7 @@ class Phase_space(Spin_wave):
         plt.plot(x,y)
         plt.xlabel("Sx")
         plt.ylabel("Sy")
+        plt.savefig(f"TypeZ_{i+1}_SxSy.png")
         plt.show()
 
         plt.plot(z,x)
@@ -43,18 +48,69 @@ class Phase_space(Spin_wave):
         plt.ylabel("Sx")
         plt.show()
 
-        N = len(y)   #サンプル数
-        f_s = 40000  # サンプリングレート f_s[Hz] (任意)
-        dt = 1 / f_s  # サンプリング周期 dt[s]
+        #plt.show()
 
-        y_fft = np.fft.fft(y)  # 離散フーリエ変換
-        freq = np.fft.fftfreq(N, d=dt)  # 周波数を割り当てる（※後述）
-        Amp = abs(y_fft / (N / 2))  # 音の大きさ（振幅の大きさ）
-        plt.plot(freq[1:int(N / 2)], Amp[1:int(N / 2)])  # A-f グラフのプロット
-        plt.xscale("log")  # 横軸を対数軸にセット
+    def make_3d(self,i):
+        t = self.t_eval
+        x = self.spin_log[i][0]
+        y = self.spin_log[i][1]
+        z = self.spin_log[i][2]
+        self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
+        self.ax.plot(x, y, z)
+        plt.show()
+
+class Sin_Phase_space(Sin_Spin_wave):
+    def __init__(self,alpha,edge_alpha,gamma,B,S0,t,t_eval,Kx,Ky,Kz,beta,Amp,omega,N,J,start,stop):
+        super().__init__(alpha,edge_alpha,gamma,B,S0,t,t_eval,Kx,Ky,Kz,beta,Amp,omega,N,J,start,stop)
+
+    def solving(self):
+        self.S0 = np.reshape(self.S0, (1, -1))
+        self.S0 = list(self.S0[0])
+        # self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
+        print(self.S0)
+
+        self.Sol = sc.integrate.solve_ivp(self.func_S, self.t, self.S0, t_eval=self.t_eval,rtol=1e-6, atol=1e-9)
+        self.S = self.Sol.y
+        self.spin_log = np.reshape(self.S, (self.N, 3, -1))
+
+
+    def make_phase_graph(self,i,ax1,ax2):      #二次元のぐらふを作る。iはi番目のスピン、axはどの軸をとるか
+        t = self.t_eval
+        x = self.spin_log[i][0]
+        y = self.spin_log[i][1]
+        z = self.spin_log[i][2]
+
+        plt.plot(x,y)
+        plt.xlabel("Sx")
+        plt.ylabel("Sy")
+        #plt.savefig(f"TypeZ_{i+1}_SxSy_{Hs}.png")
+        plt.show()
+
+        plt.plot(x, z)
+        plt.xlabel("Sx")
+        plt.ylabel("Sx")
+        plt.show()
+
+
+        plt.plot(t, x)
+        plt.xlabel("t")
+        plt.ylabel("Sx")
+        #plt.savefig(f"{i + 1}_tSx_{Hs}.png")
+        plt.show()
 
         #plt.show()
 
+    def make_3d(self,i):
+        t = self.t_eval
+        x = self.spin_log[i][0]
+        y = self.spin_log[i][1]
+        z = self.spin_log[i][2]
+        self.fig, self.ax = plt.subplots(subplot_kw=dict(projection="3d"))
+        self.ax.plot(x, y, z)
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set_zlabel("z")
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -62,12 +118,12 @@ if __name__ == '__main__':
     S0 = np.zeros((n, 3))
 
     for i in range(n):
-        S0[i][0] = 1.01
-        S0[i][2] = 0.3
+        S0[i][0] = 0.9571
+        S0[i][2] = 0.259
 
 
-    t = [0,100]  # t(時間)が0〜100まで動き、その時のfを求める。
-    t_eval = np.linspace(*t, 20000)
+    t = [0,30]  # t(時間)が0〜100まで動き、その時のfを求める。
+    t_eval = np.linspace(*t, 200000)
 
     mu_0 = 1.2
     gamma = 2.8
@@ -80,11 +136,31 @@ if __name__ == '__main__':
     Hso = h_div_2e[1] + theta[1] + j[1] - (sta_M[1] + d[1])
     Hs = Hsn * (10 ** Hso) * 1000 * (mu_0 / 1200000) * mu_0  # 最後の1000はmTにするため
 
-    pulse = []
-    for i in range(1):
-        tempul = [3 + i, 3 + i + 2, [0, -Hs, 0]]
-        pulse.append(tempul)
+    """
+    sinspin = Sin_Phase_space(0.0001, 0.1, gamma, [0, 0, mu_0 * 12], S0, t, t_eval, mu_0 * 4, 0, - mu_0 * 41.6, 0,
+                         [0, Hs, 0], [0, 1, 0], n, 1, 0, 100)
+    sinspin.solving()
+    sinspin.make_phase_graph(24, 1, 2)
+    sinspin.make_phase_graph(49, 1, 2)
+    sinspin.make_phase_graph(99, 1, 2)
+    sinspin.make_3d(24)
+    sinspin.make_3d(49)
+    sinspin.make_3d(99)
+    """
+    S0 = np.zeros((n, 3))
 
+    for i in range(n):
+        S0[i][2] = 1
 
-    spin = Phase_space(0.0001, gamma, [0, 0, mu_0 * 12], S0, t, t_eval, mu_0 * 4,0,- mu_0 * 41.6, 0, pulse, n, 1)
-    spin.make_phase_graph(50,1,2)
+    spin = Sin_Phase_space(0.0001, 0.1, gamma, [mu_0 * 4, 0, 0], S0, t, t_eval, 0, 0,  mu_0 * 220, 0,
+                           [0, Hs, 0], [0, 4, 0], n, 1, 0, 100)
+
+    spin.solving()
+    spin.make_phase_graph(0,1,2)
+    spin.make_phase_graph(24, 1, 2)
+    spin.make_phase_graph(49, 1, 2)
+    spin.make_phase_graph(99, 1, 2)
+    spin.make_3d(0)
+    spin.make_3d(24)
+    spin.make_3d(49)
+    spin.make_3d(99)
